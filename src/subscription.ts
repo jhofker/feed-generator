a12e4576fb5be52ff2595e799a42148cd289ff32
Jordan Hofker <jhofker@gmail.com>
2025-04-03 20:27:55 -0500

    lint

--- src/subscription.ts
+++ src/subscription.ts
@@ -23,7 +23,7 @@ const feedFilters = {
     'pillen', // Current NE governor
     '#nepol',
     '#neleg',
-  ]
+  ],
 };
 
 const excludes = [];
@@ -35,7 +35,7 @@ export class FirehoseSubscription extends FirehoseSubscriptionBase {
     const ops = await getOpsByType(evt);
 
     const postsToDelete = ops.posts.deletes.map((del) => del.uri);
-    
+
     // Process posts for each feed type
     let allPostsToCreate: Array<{
       uri: string;
@@ -43,26 +43,27 @@ export class FirehoseSubscription extends FirehoseSubscriptionBase {
       indexedAt: string;
       feed_type: string;
     }> = [];
-    
+
     for (const [feedType, filters] of Object.entries(feedFilters)) {
       const filteredPosts = ops.posts.creates.filter((create) => {
         const text = create.record.text.toLowerCase();
-        
+
         return (
           !excludes.some((e) => text.includes(e)) &&
           filters.some((f) => text.includes(f.toLowerCase()))
         );
       });
-      
+
       const postsToCreate = filteredPosts.map((create) => {
+        console.log(create);
         return {
           uri: create.uri,
           cid: create.cid,
           indexedAt: new Date().toISOString(),
-          feed_type: feedType
+          feed_type: feedType,
         };
       });
-      
+
       allPostsToCreate = [...allPostsToCreate, ...postsToCreate];
     }
 
@@ -72,7 +73,7 @@ export class FirehoseSubscription extends FirehoseSubscriptionBase {
         .where('uri', 'in', postsToDelete)
         .execute();
     }
-    
+
     if (allPostsToCreate.length > 0) {
       await this.db
         .insertInto('post')
